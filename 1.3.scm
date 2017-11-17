5a39e5fa94a67f4b0ed3814b7462e1c4e2e6f360
user <user@localhost.localdomain>
2017-11-18 00:19:49 +0700

    ex

--- 1.3.scm
+++ 1.3.scm
@@ -1,8 +1,14 @@
 (newline)
-    (define (sum_two_sq a b c) 
-      (+ 
-          (cond ((> a b) (* a a)) (else (* b b)))
-          (cond ((> a c) (* a a)) (else (* c c)))
-      ))
-    (write (sum_two_sq 5 2 3))
+
+(define (sum_two_sq a b c) 
+    (define (square x) (* x x))
+    (define max (if (> a b) a b))
+      ( +
+      (square max)
+      (if (or (> c a) (> c b)) (square c) 
+        (square (if (= max b) a b))
+    )))
+(write (sum_two_sq 3 0 3))
+    
+(newline)
 (exit)
